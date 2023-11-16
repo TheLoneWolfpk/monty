b2a5cb64e18043e2919333040c208aa1344b8e08
Emmanuel Owusu Asante <eo.asante3@gmail.com>
2023-11-16 15:19:28 -0800

    updating main.c

--- main.c
+++ main.c
@@ -1,9 +1,5 @@
-#define _POSIX_C_SOURCE 200809L
-
 #include "monty.h"
 #include <stdio.h>
-
-bus_w bus = {NULL, NULL, NULL, 0};
 
 /**
  * main - entry point
@@ -13,36 +9,41 @@ bus_w bus = {NULL, NULL, NULL, 0};
  */
 int main(int argc, char *argv[])
 {
-	char *cont;
-	FILE *file;
-	size_t sz = 0;
-	ssize_t rd_line = 1;
-	stack_t *stack = NULL;
-	unsigned int count = 0;
-
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
+
+	FILE *file = fopen(argv[1], "r");
+
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
+	size_t sz = 0;
+	ssize_t rd_line = 1;
+	stack_t *stack = NULL;
+	unsigned int count = 0;
+
 	while (rd_line > 0)
 	{
-		cont = NULL;
+		char *cont = NULL;
+
 		rd_line = getline(&cont, &sz, file);
-		bus.cont = cont;
 		count++;
+
 		if (rd_line > 0)
-		execut(cont, &stack, count, file);
+			execut(cont, &stack, count, file);
+
 		free(cont);
 	}
+
 	f_stack(stack);
 	fclose(file);
+
 	return (0);
 }
+
